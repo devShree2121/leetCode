69c850ec56ffe63afcb8de3591adb1b4ec27b0f9
devShree2121 <shishirregmi57@gmail.com>
2024-09-11 22:21:46 -0400

    dupliacate paranthesis function added

--- Stacks/validParanthesis.java
+++ Stacks/validParanthesis.java
@@ -32,9 +32,38 @@ public class validParanthesis {
             return false;
     
         }
+
+        public static boolean isDuplicateParanthesis(String s){
+
+            Stack<Character> myStack=new Stack<>();
+
+            for(int i=0;i<s.length();i++){
+
+                char ch=s.charAt(i);
+
+                if(ch==')'){
+                    int count=0;
+                    
+                    while(!myStack.isEmpty() && myStack.peek()!='(' ){
+
+                        myStack.pop();
+                        count++;
+                    }
+                   if(!myStack.isEmpty()){ myStack.pop();}
+                    if(count<1){return true;}
+
+                }
+                else{
+                    myStack.push(ch);
+                }
+            }
+            return false;
+        }
         public static void main(String[] args) {
-            String s="(()))))";
-            System.out.println(isValid(s));
+            String s1="(a+b)";
+            String s2="([{})";
+            System.out.println(isDuplicateParanthesis(s1));
+            System.out.println(isValid(s2));
         }
     }
 
